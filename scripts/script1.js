364fed5817c9e592eeaa7fca31cbc065f4a42996
cmkshaw <caroline.shaw@gmail.com>
2014-11-23 19:40:29 -0500

    Adding files to repo

---
+++ scripts/script1.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function(e) {
+
+ $("#toggle").click(function () { //shows p2 with a button
+      $("p#two").slideToggle("fast");
+	  
+});
+$("#show2").click(function () { //shows p2 with a button
+      $("p#three").show("fast");
+	  
+});
+
+
+});
